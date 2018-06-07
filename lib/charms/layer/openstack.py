24cf001c7a6aca36c59a8d8ea783ee723b19d67d
Cory Johns <johnsca@gmail.com>
2018-06-07 17:18:47 -0400

    Remove creating user; assume juju creds are already limited

--- lib/charms/layer/openstack.py
+++ lib/charms/layer/openstack.py
@@ -1,14 +1,8 @@
 import os
-import random
-import string
 import subprocess
 from base64 import b64decode
 
 import yaml
-from novaclient import client as novaclient_client
-from keystoneclient.v3 import client as keystoneclient_v3
-from keystoneauth1.session import Session
-from keystoneauth1.identity.v3 import Password
 
 from charmhelpers.core import hookenv
 from charmhelpers.core.unitdata import kv
@@ -46,7 +40,6 @@ def get_credentials():
         creds = yaml.load(result.stdout.decode('utf8'))
         creds_data = creds
         _save_creds(creds_data)
-        _create_project_user()
         return True
     except FileNotFoundError:
         pass  # juju trust not available
@@ -60,7 +53,6 @@ def get_credentials():
         try:
             creds_data = b64decode(config['credentials']).decode('utf8')
             _save_creds(creds_data)
-            _create_project_user()
             return True
         except Exception:
             status.blocked('invalid value for credentials config')
@@ -72,12 +64,11 @@ def get_credentials():
 
 
 def get_user_credentials():
-    return kv().get('charm.openstack.user-creds')
+    return _load_creds()
 
 
 def cleanup():
-    _delete_project_user()
-
+    pass
 
 # Internal helpers
 
@@ -96,56 +87,3 @@ def _save_creds(creds_data):
 
 def _load_creds():
     return kv().get('charm.openstack.full-creds')
-
-
-def _get_keystone_client():
-    auth = Password(**_load_creds())
-    session = Session(auth=auth, verify=False)
-    keystone = keystoneclient_v3.Client(session=session)
-    keystone.auth_ref = auth.get_access(session)
-    return keystone
-
-
-def _get_nova_client():
-    auth = Password(**_load_creds())
-    session = Session(auth=auth, verify=False)
-    nova = novaclient_client.Client(2, session=session)
-    return nova
-
-
-def _project_user_username():
-    model_uuid_prefix = os.environ['JUJU_MODEL_UUID'].split('-')[0]
-    unit_name = hookenv.local_unit().replace('/', '-')
-    return 'juju-charm-{}-{}'.format(model_uuid_prefix, unit_name)
-
-
-def _create_project_user():
-    """
-    Create a (slightly) more limited user in the project.
-    """
-    client = _get_keystone_client()
-    alphabet = string.ascii_letters + string.digits
-    full_creds = _load_creds()
-    username = _project_user_username()
-    password = ''.join(random.choice(alphabet) for _ in range(20))
-    client.users.create(
-        name=username,
-        password=password,
-        domain=full_creds['project_domain_name'],
-        default_project=full_creds['project_name'],
-    )
-    kv().set('charm.openstack.user-creds', dict(
-        auth_url=full_creds['endpoint'],
-        username=username,
-        password=password,
-        user_domain_name=full_creds['user_domain_name'],
-        project_domain_name=full_creds['project_domain_name'],
-        project_name=full_creds['tenant_name'],
-    ))
-
-
-def _delete_project_user():
-    client = _get_keystone_client()
-    username = _project_user_username()
-    client.users.delete(username)
-    kv().unset('charm.openstack.user-creds')
