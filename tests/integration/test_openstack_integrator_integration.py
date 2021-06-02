2a668bc4abb97dffb668d31efb1e9b7cf01c4ef2
Robert Gildein <robert.gildein@canonical.com>
2021-06-02 12:12:39 +0200

    * update bundle + avoid forcing to use LXD * remove redundant juju requirements from tox.ini (installed with pytest-operator) * add .jujuignore

--- tests/integration/test_openstack_integrator_integration.py
+++ tests/integration/test_openstack_integrator_integration.py
@@ -8,10 +8,10 @@ log = logging.getLogger(__name__)
 
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test):
+    """Build and deploy openstack-integrator in bundle"""
+    openstack_integrator = await ops_test.build_charm(".")
     bundle = ops_test.render_bundle(
-        "tests/data/bundle.yaml",
-        master_charm=await ops_test.build_charm("."),
-        series="focal"
+        "tests/data/bundle.yaml", master_charm=openstack_integrator, series="focal"
     )
     await ops_test.model.deploy(bundle, trust=True)
     await ops_test.model.wait_for_idle(wait_for_active=True, timeout=60 * 60)
