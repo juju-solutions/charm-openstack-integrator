5f98d015fd39ee2322a5b1099da60bede24116a2
Adam Dyess <adam.dyess@canonical.com>
2023-06-20 10:33:43 -0500

    Address testing lint issues

--- tests/integration/test_openstack_integrator_integration.py
+++ tests/integration/test_openstack_integrator_integration.py
@@ -8,8 +8,10 @@ log = logging.getLogger(__name__)
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test):
     """Build and deploy openstack-integrator in bundle."""
-    bundle, = await ops_test.async_render_bundles(ops_test.Bundle("kubernetes-core", channel="edge"))
+    bundle = ops_test.Bundle("kubernetes-core", channel="edge")
+    bundle, = await ops_test.async_render_bundles(bundle)
     log.info("Deploy Kubernetes Core...")
+
     deploy = shlex.split(f"juju deploy -m {ops_test.model_full_name} {bundle}")
     rc, stdout, stderr = await ops_test.run(*deploy)
     assert rc == 0, f"Bundle deploy failed: {(stderr or stdout).strip()}"
@@ -17,8 +19,8 @@ async def test_build_and_deploy(ops_test):
 
     await ops_test.model.wait_for_idle(wait_for_active=True, timeout=60 * 60)
 
-    openstack_integrator = await ops_test.build_charm(".")
-    await ops_test.model.deploy("local:" + str(openstack_integrator), to="lxd:0", trust=True)
+    charm = await ops_test.build_charm(".")
+    await ops_test.model.deploy(f"local:{charm}", to="lxd:0", trust=True)
     await ops_test.model.wait_for_idle(wait_for_active=True, timeout=10 * 60)
 
 
