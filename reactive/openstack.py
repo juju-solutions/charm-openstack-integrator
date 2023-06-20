f5fec138795e67b50edc7db602f96201f8e317e8
Adam Dyess <adam.dyess@canonical.com>
2023-06-20 10:45:10 -0500

    Just black (#70)
    * Deprecate using snaps as resources in favor of using a 'snap-store-proxy'
    
    * Just Black
    
    ---------
    
    Co-authored-by: Kevin W Monroe <kevin.monroe@canonical.com>
    Co-authored-by: cdkbot <cdkbot@juju.solutions>

--- reactive/openstack.py
+++ reactive/openstack.py
@@ -15,134 +15,142 @@ from charms.reactive.relations import endpoint_from_name
 from charms import layer
 
 
-@when_all('snap.installed.openstackclients')
+@when_all("snap.installed.openstackclients")
 def set_app_ver():
-    version = layer.snap.get_installed_version('openstackclients')
+    version = layer.snap.get_installed_version("openstackclients")
     hookenv.application_version_set(version)
 
 
-@when_any('config.changed.credentials',
-          'config.changed.auth-url',
-          'config.changed.username',
-          'config.changed.password',
-          'config.changed.project-name',
-          'config.changed.user-domain-name',
-          'config.changed.project-domain-name',
-          'config.changed.region',
-          'config.changed.endpoint-tls-ca')
+@when_any(
+    "config.changed.credentials",
+    "config.changed.auth-url",
+    "config.changed.username",
+    "config.changed.password",
+    "config.changed.project-name",
+    "config.changed.user-domain-name",
+    "config.changed.project-domain-name",
+    "config.changed.region",
+    "config.changed.endpoint-tls-ca",
+)
 def update_creds():
-    clear_flag('charm.openstack.creds.set')
+    clear_flag("charm.openstack.creds.set")
 
 
-@hook('upgrade-charm')
+@hook("upgrade-charm")
 def upgrade_charm():
     # when the charm is upgraded, recheck the creds in case anything
     # has changed or we want to handle any of the fields differently
-    clear_flag('charm.openstack.creds.set')
+    clear_flag("charm.openstack.creds.set")
 
 
-@hook('update-status')
+@hook("update-status")
 def update_status():
     # need to recheck creds in case the credentials from Juju have changed
-    clear_flag('charm.openstack.creds.set')
+    clear_flag("charm.openstack.creds.set")
 
 
-@hook('pre-series-upgrade')
+@hook("pre-series-upgrade")
 def pre_series_upgrade():
-    layer.status.blocked('Series upgrade in progress')
+    layer.status.blocked("Series upgrade in progress")
 
 
-@when_not('charm.openstack.creds.set')
+@when_not("charm.openstack.creds.set")
 def get_creds():
     prev_creds = layer.openstack.get_credentials()
     credentials_exist = layer.openstack.update_credentials()
-    toggle_flag('charm.openstack.creds.set', credentials_exist)
+    toggle_flag("charm.openstack.creds.set", credentials_exist)
     creds = layer.openstack.get_credentials()
     if creds != prev_creds:
-        set_flag('charm.openstack.creds.changed')
+        set_flag("charm.openstack.creds.changed")
 
 
-@when_all('snap.installed.openstackclients',
-          'charm.openstack.creds.set')
-@when_not('endpoint.clients.requests-pending')
-@when_not('upgrade.series.in-progress')
+@when_all("snap.installed.openstackclients", "charm.openstack.creds.set")
+@when_not("endpoint.clients.requests-pending")
+@when_not("upgrade.series.in-progress")
 def no_requests():
-    layer.status.active('Ready')
+    layer.status.active("Ready")
 
 
-@when_all('snap.installed.openstackclients',
-          'charm.openstack.creds.set',
-          'endpoint.clients.joined')
-@when_any('endpoint.clients.requests-pending',
-          'config.changed', 'charm.openstack.creds.changed')
-@when_not('upgrade.series.in-progress')
+@when_all(
+    "snap.installed.openstackclients",
+    "charm.openstack.creds.set",
+    "endpoint.clients.joined",
+)
+@when_any(
+    "endpoint.clients.requests-pending",
+    "config.changed",
+    "charm.openstack.creds.changed",
+)
+@when_not("upgrade.series.in-progress")
 def handle_requests():
-    layer.status.maintenance('Granting integration requests')
-    clients = endpoint_from_name('clients')
-    config_change = is_flag_set('config.changed')
+    layer.status.maintenance("Granting integration requests")
+    clients = endpoint_from_name("clients")
+    config_change = is_flag_set("config.changed")
     config = hookenv.config()
     has_octavia = layer.openstack.detect_octavia()
     try:
-        manage_security_groups = strtobool(config['manage-security-groups'])
+        manage_security_groups = strtobool(config["manage-security-groups"])
         # use bool() to force True / False instead of 1 / 0
         manage_security_groups = bool(manage_security_groups)
     except ValueError:
-        layer.status.blocked('Invalid value for manage-security-groups config')
+        layer.status.blocked("Invalid value for manage-security-groups config")
         return
     except AttributeError:
         # in case manage_security_groups is already bool
-        manage_security_groups = config['manage-security-groups']
-    creds_changed = is_flag_set('charm.openstack.creds.changed')
+        manage_security_groups = config["manage-security-groups"]
+    creds_changed = is_flag_set("charm.openstack.creds.changed")
     refresh_requests = config_change or creds_changed
     requests = clients.all_requests if refresh_requests else clients.new_requests
     for request in requests:
-        layer.status.maintenance(
-            'Granting request for {}'.format(request.unit_name))
+        layer.status.maintenance("Granting request for {}".format(request.unit_name))
         creds = layer.openstack.get_credentials()
         request.set_credentials(**creds)
-        request.set_lbaas_config(config['subnet-id'],
-                                 config['floating-network-id'],
-                                 config['lb-method'],
-                                 manage_security_groups,
-                                 has_octavia,
-                                 lb_enabled=config['lb-enabled'],
-                                 internal_lb=config['internal-lb'])
+        request.set_lbaas_config(
+            config["subnet-id"],
+            config["floating-network-id"],
+            config["lb-method"],
+            manage_security_groups,
+            has_octavia,
+            lb_enabled=config["lb-enabled"],
+            internal_lb=config["internal-lb"],
+        )
 
         def _or_none(val):
-            if val in (None, '', 'null'):
+            if val in (None, "", "null"):
                 return None
             else:
                 return val
+
         request.set_block_storage_config(
-            _or_none(config.get('bs-version')),
-            _or_none(config.get('trust-device-path')),
-            _or_none(config.get('ignore-volume-az')))
-        layer.openstack.log('Finished request for {}', request.unit_name)
+            _or_none(config.get("bs-version")),
+            _or_none(config.get("trust-device-path")),
+            _or_none(config.get("ignore-volume-az")),
+        )
+        layer.openstack.log("Finished request for {}", request.unit_name)
     clients.mark_completed()
-    clear_flag('charm.openstack.creds.changed')
+    clear_flag("charm.openstack.creds.changed")
 
 
-@when_all('charm.openstack.creds.set',
-          'credentials.connected')
-@when_not('upgrade.series.in-progress')
+@when_all("charm.openstack.creds.set", "credentials.connected")
+@when_not("upgrade.series.in-progress")
 def write_credentials():
-    credentials = endpoint_from_name('credentials')
+    credentials = endpoint_from_name("credentials")
     reformatted_creds = layer.openstack.get_creds_and_reformat()
     credentials.expose_credentials(reformatted_creds)
 
 
-@when_all('charm.openstack.creds.set',
-          'endpoint.loadbalancer.joined')
-@when_not('upgrade.series.in-progress')
+@when_all("charm.openstack.creds.set", "endpoint.loadbalancer.joined")
+@when_not("upgrade.series.in-progress")
 def create_or_update_loadbalancers():
-    layer.status.maintenance('Managing load balancers')
-    lb_clients = endpoint_from_name('loadbalancer')
+    layer.status.maintenance("Managing load balancers")
+    lb_clients = endpoint_from_name("loadbalancer")
     try:
         for request in lb_clients.requests:
             if not request.members:
                 continue
-            lb = layer.openstack.manage_loadbalancer(request.application_name,
-                                                     request.members)
+            lb = layer.openstack.manage_loadbalancer(
+                request.application_name, request.members
+            )
             request.set_address_port(lb.fip or lb.address, lb.port)
     except layer.openstack.OpenStackError as e:
         layer.status.blocked(str(e))
