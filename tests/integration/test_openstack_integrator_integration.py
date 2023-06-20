f2426f297f7f252b283544aa9f96bbec7e7b44b3
Adam Dyess <adam.dyess@canonical.com>
2023-06-20 10:31:49 -0500

    Updating integration tests to operate with juju==3.0.4

--- tests/integration/test_openstack_integrator_integration.py
+++ tests/integration/test_openstack_integrator_integration.py
@@ -1,6 +1,6 @@
 import logging
-
 import pytest
+import shlex
 
 log = logging.getLogger(__name__)
 
@@ -8,12 +8,17 @@ log = logging.getLogger(__name__)
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test):
     """Build and deploy openstack-integrator in bundle."""
-    bundle = ops_test.render_bundle("tests/data/bundle.yaml", series="focal")
-    await ops_test.model.deploy(bundle)
+    bundle, = await ops_test.async_render_bundles(ops_test.Bundle("kubernetes-core", channel="edge"))
+    log.info("Deploy Kubernetes Core...")
+    deploy = shlex.split(f"juju deploy -m {ops_test.model_full_name} {bundle}")
+    rc, stdout, stderr = await ops_test.run(*deploy)
+    assert rc == 0, f"Bundle deploy failed: {(stderr or stdout).strip()}"
+    log.info(stdout)
+
     await ops_test.model.wait_for_idle(wait_for_active=True, timeout=60 * 60)
 
     openstack_integrator = await ops_test.build_charm(".")
-    await ops_test.model.deploy(openstack_integrator, to="lxd:0", trust=True)
+    await ops_test.model.deploy("local:" + str(openstack_integrator), to="lxd:0", trust=True)
     await ops_test.model.wait_for_idle(wait_for_active=True, timeout=10 * 60)
 
 
